df2fb63982e55a23ee76590acc1b2ef18998a555
oecchi <65586213+oecchi@users.noreply.github.com>
2020-06-14 16:39:59 +0900

    tweet&logo

--- db/migrate/20200611033908_create_tweets.rb
+++
@@ -1,9 +0,0 @@
-class CreateTweets < ActiveRecord::Migration[5.2]
-  def change
-    create_table :tweets do |t|
-      t.string :body
-      t.string :image
-      t.timestamps
-    end
-  end
-end
