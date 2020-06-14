a6bbdc6fdac3bf6666842256bf6a984c554af4be
oecchi <65586213+oecchi@users.noreply.github.com>
2020-06-14 13:57:28 +0900

    とりあえず

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,18 @@
 class TweetsController < ApplicationController
+
   def index
+    @tweet = Tweet.new
   end
+
+  def create
+    Tweet.create(tweet_params)
+      # binding.pry
+    redirect_to root_path
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:body, :image)
+  end
+
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ApplicationRecord
 
+  validates :body, presence: true, unless: :image?
+  mount_uploader :image, ImageUploader
 end
